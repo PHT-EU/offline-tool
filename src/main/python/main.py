31b8fc1fc6a30bbf6ac87f4ca1bda90ee1b20a81
tyrast <tyra.stickel@student.uni-tuebingen.de>
2021-11-03 09:49:25 +0100

    Change requirements

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -8,7 +8,6 @@ from functionality import primes
 from functionality.primes import PrivateKey, PublicKey
 from functionality.SecurityValuesFunctionality import SecurityValuesFunctionality
 from functionality.SecureAddtionFunctionality import SecureAdditionFunctionality
-from PySide2 import QtXml
 
 import sys, platform
 from visualisation.label_dictionary import *
